6e08a6506bbfac9b3750a5f253cdaa4c5e01bab9
mariam-codes256 <alizoymarie@gmail.com>
2025-06-14 02:00:22 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SnackLab.WebApp.Startup))]
+
+namespace SnackLab.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+            ConfigureAuth(app);
+        }
+    }
+}
